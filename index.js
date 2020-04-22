96b0851a951dae89622657198810478399aefe08
wolfwithcode <wolfwithcode@gmail.com>
2020-04-22 14:39:04 +0700

    Install nodemon

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ app.post('/api/users/register', (req, res) => {
 
     const user = new User(req.body);
     user.save((err, userData) => {
-        if(err) return res.json({success: false, err})
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        });
     })
-    return res.status(200);
+         
 })
 
 
@@ -29,9 +32,9 @@ app.post('/api/users/register', (req, res) => {
 
 
 
-// app.get('/', (req, res)=>{
-//     res.send('hello world')
-// })
+app.get('/', (req, res)=>{
+    res.send('hello world')
+})
 
 
 app.listen(5000);
