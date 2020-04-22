d1e8b2fdb96a83f7448aba49a56c6bd97c3b0a3f
wolfwithcode <wolfwithcode@gmail.com>
2020-04-22 14:29:46 +0700

    Register function using postman

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {User} = require('./models/user');
+
+
 mongoose.connect('mongodb+srv://admin:admin123@cluster0-svt6p.mongodb.net/test?retryWrites=true&w=majority',
 {useNewUrlParser: true}).then(() => console.log('DB connected'))
                         .catch(err => console.error(err));
 
-app.get('/', (req, res)=>{
-    res.send('hello world')
+
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json({success: false, err})
+    })
+    return res.status(200);
 })
+
+
+
+
+
+
+// app.get('/', (req, res)=>{
+//     res.send('hello world')
+// })
+
+
 app.listen(5000);
