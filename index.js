6c77924e8f2d8ba859239afe92ddac81fa5318bd
wolfwithcode <wolfwithcode@gmail.com>
2020-04-22 16:58:00 +0700

    Lesson 13 : Login function with jsonwebtoken

--- index.js
+++ index.js
@@ -20,23 +20,47 @@ app.use(cookieParser());
 app.post('/api/users/register', (req, res) => {
 
     const user = new User(req.body);
-    user.save((err, userData) => {
+    user.save((err, doc) => {
         if(err) return res.json({success: false, err});
-        return res.status(200).json({
-            success: true
+        res.status(200).json({
+            success: true,
+            userData: doc
         });
     })
          
 })
 
-
-
-
-
-
-app.get('/', (req, res)=>{
-    res.send('hello world')
+app.post('/api/user/login',(req, res) => {
+    //find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user)
+        return res.json({
+            loginSuccess: false,
+            message: "Auth failed, email not found"
+        });
+        // compare password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({loginSuccess: false, message: "wrong password"})
+            }
+        })
+        //generate token 
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true
+                })
+        })
+    })
 })
+
+// app.get('/', (req, res)=>{
+//     res.send('hello world')
+// })
+
+
 
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+
 const userSchema = mongoose.Schema({
     name: {
         type:String,
@@ -47,6 +50,24 @@ userSchema.pre('save', function(next){
     }
     
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secret');
+
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
