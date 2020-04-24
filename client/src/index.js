2312980ed86d9036ae3b056645e79f65976a8eec
wolfwithcode <wolfwithcode@gmail.com>
2020-04-24 14:51:46 +0700

    Lesson 22: Setting up REDUX

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,21 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./reducers";
+
+const createStoreWithMiddleware = applyMiddware(promiseMiddleware, ReduxThunk)(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-   <App />
-  </BrowserRouter>
+
+  <Provider store={ createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ) }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider> 
  ,
   document.getElementById('root')
 );
